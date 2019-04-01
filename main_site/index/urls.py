0de79969bc92c3bcf9f347ed565451604a2b2302
Robert Butler <mr.robert.g.butler@gmail.com>
2019-03-31 21:27:12 -0700

    Added sidebar and updated navbar.

--- main_site/index/urls.py
+++ main_site/index/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(route='', view=views.index, name='index'),
+    path(route='', view=views.index, name='index-home'),
+    path(route='about/', view=views.about, name='index-about'),
 ]
 
--- main_site/index/views.py
+++ main_site/index/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request=request, template_name='index/index.html')
+    return render(
+        request=request, template_name='index/home.html', context={}
+    )
+
+def about(request):
+    return render(
+        request=request, template_name='index/about.html', context={}
+    )
